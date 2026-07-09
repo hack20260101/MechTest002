4dc01e542d03914b9c777e225340a1c0783bbd72
hack20260101 <hackathon20260101@gmail.com>
2026-07-10 01:06:41 +0530

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -2,15 +2,16 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react-swc";
 import path from "path";
 
-// https://vitejs.dev/config/
 export default defineConfig({
+  base: "/DrawValidatorPro/",   // <-- Repository name
+
   server: {
     host: "::",
     port: 8080,
   },
-  plugins: [
-    react(),
-  ],
+
+  plugins: [react()],
+
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
